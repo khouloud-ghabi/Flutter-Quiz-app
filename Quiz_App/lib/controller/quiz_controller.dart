65968acbc8ff675bc95f6b8f98c8888777c4f4c2
Khouloud <80363956+khouloud-ghabi@users.noreply.github.com>
2024-07-13 16:46:49 +0100

    commit

--- Quiz_App/lib/controller/quiz_controller.dart
+++ Quiz_App/lib/controller/quiz_controller.dart
@@ -4,6 +4,68 @@ import 'package:quiz_app/model/question.dart';
 class QuizController extends GetxController {
   String name = '';
   final List<QuestionModel> _questionList = [
-    QuestionModel(id:id ,answer: answer, question: question, options: options)
+    QuestionModel(
+      id: 1,
+      question:
+          "Best Channel for Flutter ",
+      answer: 2,
+      options: ['Sec it', 'Sec it developer', 'sec it developers', 'mesh sec it '],
+    ),
+    QuestionModel(
+      id: 2,
+      question: "Best State Mangment Ststem is ",
+      answer: 1,
+      options: ['BloC', 'GetX', 'Provider', 'riverPod'],
+    ),
+    QuestionModel(
+      id: 3,
+      question: "Best Flutter dev",
+      answer: 2,
+      options: ['sherif', 'sherif ahmed', 'ahmed sherif', 'doc sherif'],
+    ),
+    QuestionModel(
+      id: 4,
+      question: "Sherif is",
+      answer: 1,
+      options: ['eng', 'Doc', 'eng/Doc', 'Doc/Eng'],
+    ),
+    QuestionModel(
+      id: 5,
+      question:
+          "Best Rapper in Egypt",
+      answer: 3,
+      options: ['Eljoker', 'Abyu', 'R3', 'All of the above'],
+    ),
+    QuestionModel(
+      id: 6,
+      question: "Real Name of ahmed sherif",
+      answer: 2,
+      options: ['ahmed sherif', 'sherif', 'Haytham', 'NONE OF ABOVE'],
+    ),
+    QuestionModel(
+      id: 7,
+      question: "Sherif love",
+      answer: 3,
+      options: ['Pharma', 'Micro', 'Medicnal', 'NONE OF ABOVE'],
+    ),
+    QuestionModel(
+      id: 8,
+      question: "hello",
+      answer: 3,
+      options: ['hello', 'hi', 'hola', 'Suiiiiiiiiiiii'],
+    ),
+    QuestionModel(
+      id: 9,
+      question:
+      "Best Channel for Flutter ",
+      answer: 2,
+      options: ['Sec it', 'Sec it developer', 'sec it developers', 'mesh sec it '],
+    ),
+    QuestionModel(
+      id: 10,
+      question: "Best State Mangment Ststem is ",
+      answer: 1,
+      options: ['BloC', 'GetX', 'Provider', 'riverPod'],
+    ),
   ];
 }
